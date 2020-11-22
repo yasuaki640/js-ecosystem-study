6c788679db4212849eb32c044f6550ec057250cf
yasuaki640 <yasuaki640@gmail.com>
2020-11-22 15:38:52 +0900

    call news api

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 'use strict';
 
 const express = require('express');
+
+const dotenv = require('dotenv');
+dotenv.config({path: './.env'});
+
+const NewsApi = require('newsapi');
+const newsapi = new NewsApi(process.env.NEWS_API_ACCESS_KEY);
+
