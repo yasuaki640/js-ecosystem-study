942f0251ea28d497508c3702cfbdc6012c21d638
yasuaki640 <yasuaki640@gmail.com>
2020-11-22 15:14:12 +0900

    delete hello world app code

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
 'use strict';
 
 const express = require('express');
-
-const PORT = 3000;
-const HOST = '0.0.0.0';
-
-//app codes
-const app = express();
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
