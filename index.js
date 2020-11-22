b75ff9ffa72c6daead3e4d71dad7dea9f2c1e14b
yasuaki640 <yasuaki640@gmail.com>
2020-11-22 15:53:12 +0900

    output news in console

--- index.js
+++ index.js
@@ -8,3 +8,10 @@ dotenv.config({path: './.env'});
 const NewsApi = require('newsapi');
 const newsapi = new NewsApi(process.env.NEWS_API_ACCESS_KEY);
 
+newsapi.v2.topHeadlines({
+    q: '新型コロナ',
+    category: 'business',
+    language: 'ja'
+}).then(news => console.log(news));
+
+
