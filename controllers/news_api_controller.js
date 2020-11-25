bd72948e40009ac266771c43b3031ec45d6e6921
yasuaki640 <yasuaki640@gmail.com>
2020-11-25 19:51:45 +0900

    def api controller

--- controllers/news_api_controller.js
+++ controllers/news_api_controller.js
@@ -1,2 +1,16 @@
 const NewsApi = require('newsapi');
 const newsapi = new NewsApi(process.env.NEWS_API_ACCESS_KEY);
+
+exports.index = function (req, res, next) {
+    newsapi.v2.topHeadlines({
+        country: 'jp'
+    }).then(news => res.add(news));
+};
+
+exports.getTopLines = function (req, res, next, query, category) {
+    newsapi.v2.topHeadlines({
+        q: query,
+        category: category,
+        country: 'jp'
+    }).then(news => res.add(news));
+};
