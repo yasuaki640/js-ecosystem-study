82cbce8b136bd517999a7f2f77d8e5c81b2c8623
yasuaki640 <yasuaki640@gmail.com>
2020-11-25 19:52:46 +0900

    delete sample code

--- server.js
+++ server.js
@@ -5,13 +5,5 @@ const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config({path: './.env'});
 
-const NewsApi = require('newsapi');
-const newsapi = new NewsApi(process.env.NEWS_API_ACCESS_KEY);
-
-newsapi.v2.topHeadlines({
-    q: '新型コロナ',
-    category: 'business',
-    language: 'ja'
-}).then(news => console.log(news));
 
 
