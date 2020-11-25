cd58a5108a5569e23f563ff18d63397519dddda9
yasuaki640 <yasuaki640@gmail.com>
2020-11-25 19:52:37 +0900

    def index page api controller

--- routes/index.js
+++ routes/index.js
@@ -1,2 +1,7 @@
 let express = require('express');
 let router = express.Router();
+let controllers = require('../controllers');
+
+router.get('/', controllers.news_api_controller.index);
+
+module.exports = router;
