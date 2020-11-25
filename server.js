47b4228804bd922d74d998d947493b1a2a971e5d
yasuaki640 <yasuaki640@gmail.com>
2020-11-25 20:00:25 +0900

    call api on server

--- server.js
+++ server.js
@@ -5,5 +5,10 @@ const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config({path: './.env'});
 
+let server = express();
+
+let api = require('./routes/api');
+server.use('/_api/',api);
+
 
 
